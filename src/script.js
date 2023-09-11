cb1d8ad10070fff7d73d90883e021f28b83b2934
Maciej Łoziński <maciej@robotix-lozinski.pl>
2023-09-11 15:32:59 +0200

    urlencode page

--- src/script.js
+++ src/script.js
@@ -11,7 +11,7 @@
   const { page } = await browser.storage.sync.get("page");
   const { append } = await browser.storage.sync.get("append");
 
-  const location = `logseq://x-callback-url/quickCapture?page=${page}&append=${append}&title=${
+  const location = `logseq://x-callback-url/quickCapture?page=${encodeURIComponent(page)}&append=${append}&title=${
     encodeURIComponent(tab.title)
   }&content=${result ? encodeURIComponent(result) : ""}&url=${encodeURIComponent(tab.url)}`;
 
